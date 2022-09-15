06f44e42362df1fdfd32d5a50169ee5bd290c3fd
Pavel <Pavel@DESKTOP-DOB86IK>
2022-09-15 23:15:02 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Individual_Assignment_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Russian = new System.Windows.Forms.PictureBox();
+            this.Spanish = new System.Windows.Forms.PictureBox();
+            this.German = new System.Windows.Forms.PictureBox();
+            this.French = new System.Windows.Forms.PictureBox();
+            this.Italian = new System.Windows.Forms.PictureBox();
+            this.English = new System.Windows.Forms.PictureBox();
+            this.Portuguese = new System.Windows.Forms.PictureBox();
+            this.Estonian = new System.Windows.Forms.PictureBox();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Help = new System.Windows.Forms.Button();
+            this.Show = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.Hello = new System.Windows.Forms.Label();
+            this.SoundsLike = new System.Windows.Forms.Label();
+            this.Hide = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Russian)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Spanish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.German)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.French)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Italian)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.English)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Portuguese)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Estonian)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Russian
+            // 
+            this.Russian.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Russian.Image = ((System.Drawing.Image)(resources.GetObject("Russian.Image")));
+            this.Russian.Location = new System.Drawing.Point(220, 53);
+            this.Russian.Name = "Russian";
+            this.Russian.Size = new System.Drawing.Size(160, 90);
+            this.Russian.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Russian.TabIndex = 1;
+            this.Russian.TabStop = false;
+            this.Russian.Click += new System.EventHandler(this.Russian_Click);
+            // 
+            // Spanish
+            // 
+            this.Spanish.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Spanish.Image = ((System.Drawing.Image)(resources.GetObject("Spanish.Image")));
+            this.Spanish.Location = new System.Drawing.Point(419, 171);
+            this.Spanish.Name = "Spanish";
+            this.Spanish.Size = new System.Drawing.Size(160, 90);
+            this.Spanish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Spanish.TabIndex = 2;
+            this.Spanish.TabStop = false;
+            this.Spanish.Click += new System.EventHandler(this.Spanish_Click);
+            // 
+            // German
+            // 
+            this.German.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.German.Image = ((System.Drawing.Image)(resources.GetObject("German.Image")));
+            this.German.Location = new System.Drawing.Point(614, 53);
+            this.German.Name = "German";
+            this.German.Size = new System.Drawing.Size(160, 90);
+            this.German.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.German.TabIndex = 3;
+            this.German.TabStop = false;
+            this.German.Click += new System.EventHandler(this.German_Click);
+            // 
+            // French
+            // 
+            this.French.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.French.Image = ((System.Drawing.Image)(resources.GetObject("French.Image")));
+            this.French.Location = new System.Drawing.Point(23, 171);
+            this.French.Name = "French";
+            this.French.Size = new System.Drawing.Size(160, 90);
+            this.French.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.French.TabIndex = 4;
+            this.French.TabStop = false;
+            this.French.Click += new System.EventHandler(this.French_Click);
+            // 
+            // Italian
+            // 
+            this.Italian.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Italian.Image = ((System.Drawing.Image)(resources.GetObject("Italian.Image")));
+            this.Italian.Location = new System.Drawing.Point(220, 171);
+            this.Italian.Name = "Italian";
+            this.Italian.Size = new System.Drawing.Size(160, 90);
+            this.Italian.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Italian.TabIndex = 5;
+            this.Italian.TabStop = false;
+            this.Italian.Click += new System.EventHandler(this.Italian_Click);
+            // 
+            // English
+            // 
+            this.English.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.English.Image = ((System.Drawing.Image)(resources.GetObject("English.Image")));
+            this.English.Location = new System.Drawing.Point(23, 53);
+            this.English.Name = "English";
+            this.English.Size = new System.Drawing.Size(160, 90);
+            this.English.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.English.TabIndex = 6;
+            this.English.TabStop = false;
+            this.English.Click += new System.EventHandler(this.English_Click);
+            // 
+            // Portuguese
+            // 
+            this.Portuguese.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Portuguese.Image = ((System.Drawing.Image)(resources.GetObject("Portuguese.Image")));
+            this.Portuguese.Location = new System.Drawing.Point(614, 171);
+            this.Portuguese.Name = "Portuguese";
+            this.Portuguese.Size = new System.Drawing.Size(160, 90);
+            this.Portuguese.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Portuguese.TabIndex = 7;
+            this.Portuguese.TabStop = false;
+            this.Portuguese.Click += new System.EventHandler(this.Portuguese_Click);
+            // 
+            // Estonian
+            // 
+            this.Estonian.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Estonian.Image = ((System.Drawing.Image)(resources.GetObject("Estonian.Image")));
+            this.Estonian.Location = new System.Drawing.Point(419, 53);
+            this.Estonian.Name = "Estonian";
+            this.Estonian.Size = new System.Drawing.Size(160, 90);
+            this.Estonian.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Estonian.TabIndex = 8;
+            this.Estonian.TabStop = false;
+            this.Estonian.Click += new System.EventHandler(this.Estonian_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Exit.Location = new System.Drawing.Point(628, 393);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(160, 45);
+            this.Exit.TabIndex = 9;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Help
+            // 
+            this.Help.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Help.Location = new System.Drawing.Point(462, 393);
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(160, 45);
+            this.Help.TabIndex = 10;
+            this.Help.Text = "Help";
+            this.Help.UseVisualStyleBackColor = true;
+            this.Help.Click += new System.EventHandler(this.Help_Click);
+            // 
+            // Show
+            // 
+            this.Show.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Show.Location = new System.Drawing.Point(12, 393);
+            this.Show.Name = "Show";
+            this.Show.Size = new System.Drawing.Size(160, 45);
+            this.Show.TabIndex = 11;
+            this.Show.Text = "Show Pronunciation";
+            this.Show.UseVisualStyleBackColor = true;
+            this.Show.Click += new System.EventHandler(this.Pronunciation_Click);
+            // 
+            // Title
+            // 
+            this.Title.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Title.Location = new System.Drawing.Point(0, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(800, 30);
+            this.Title.TabIndex = 12;
+            this.Title.Text = "Hello In Different Languages";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Hello
+            // 
+            this.Hello.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Hello.Location = new System.Drawing.Point(0, 295);
+            this.Hello.Name = "Hello";
+            this.Hello.Size = new System.Drawing.Size(800, 30);
+            this.Hello.TabIndex = 13;
+            this.Hello.Text = "English: Hello";
+            this.Hello.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SoundsLike
+            // 
+            this.SoundsLike.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SoundsLike.Location = new System.Drawing.Point(0, 334);
+            this.SoundsLike.Name = "SoundsLike";
+            this.SoundsLike.Size = new System.Drawing.Size(800, 30);
+            this.SoundsLike.TabIndex = 14;
+            this.SoundsLike.Text = "Sounds like: heh · low";
+            this.SoundsLike.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.SoundsLike.Visible = false;
+            // 
+            // Hide
+            // 
+            this.Hide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Hide.Location = new System.Drawing.Point(178, 393);
+            this.Hide.Name = "Hide";
+            this.Hide.Size = new System.Drawing.Size(160, 45);
+            this.Hide.TabIndex = 15;
+            this.Hide.Text = "Hide Pronunciation";
+            this.Hide.UseVisualStyleBackColor = true;
+            this.Hide.Click += new System.EventHandler(this.Hide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Hide);
+            this.Controls.Add(this.SoundsLike);
+            this.Controls.Add(this.Hello);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.Show);
+            this.Controls.Add(this.Help);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Estonian);
+            this.Controls.Add(this.Portuguese);
+            this.Controls.Add(this.English);
+            this.Controls.Add(this.Italian);
+            this.Controls.Add(this.French);
+            this.Controls.Add(this.German);
+            this.Controls.Add(this.Spanish);
+            this.Controls.Add(this.Russian);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Russian)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Spanish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.German)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.French)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Italian)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.English)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Portuguese)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Estonian)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox Russian;
+        private System.Windows.Forms.PictureBox Spanish;
+        private System.Windows.Forms.PictureBox German;
+        private System.Windows.Forms.PictureBox French;
+        private System.Windows.Forms.PictureBox Italian;
+        private System.Windows.Forms.PictureBox English;
+        private System.Windows.Forms.PictureBox Portuguese;
+        private System.Windows.Forms.PictureBox Estonian;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Help;
+        private System.Windows.Forms.Button Show;
+        private System.Windows.Forms.Button Hide;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label Hello;
+        private System.Windows.Forms.Label SoundsLike;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Individual_Assignment_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void English_Click(object sender, EventArgs e)
+        {
+            Hello.Text = "English: Hello";
+            SoundsLike.Text = "Sounds like: heh-low";
+        }
+
+        private void Russian_Click(object sender, EventArgs e)
+        {
+            Hello.Text = "Russian: Привет";
+            SoundsLike.Text = "Sounds like: pri-vet";
+        }
+
+        private void Estonian_Click(object sender, EventArgs e)
+        {
+            Hello.Text = "Estonian: Tere";
+            SoundsLike.Text = "Sounds like: tehr-reh";
+        }
+
+        private void German_Click(object sender, EventArgs e)
+        {
+            Hello.Text = "German: Guten Tag";
+            SoundsLike.Text = "Sounds like: goot-en tahk";
+        }
+
+        private void French_Click(object sender, EventArgs e)
+        {
+            Hello.Text = "French: Bonjour";
+            SoundsLike.Text = "Sounds like: bohn-zhoohr";
+        }
+
+        private void Italian_Click(object sender, EventArgs e)
+        {
+            Hello.Text = "Italian: Salve";
+            SoundsLike.Text = "Sounds like: sahl-veh";
+        }
+
+        private void Spanish_Click(object sender, EventArgs e)
+        {
+            Hello.Text = "Spanish: Hola";
+            SoundsLike.Text = "Sounds like: oh-lah";
+        }
+
+        private void Portuguese_Click(object sender, EventArgs e)
+        {
+            Hello.Text = "Portuguese: Olá";
+            SoundsLike.Text = "Sounds like: oh-lah";
+        }
+
+        private void Pronunciation_Click(object sender, EventArgs e)
+        {
+            SoundsLike.Visible = true;
+        }
+
+        private void Hide_Click(object sender, EventArgs e)
+        {
+            SoundsLike.Visible = false;
+        }
+
+        private void Help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pavel Golovan 2022\n\nThis app allows you to learn how to say Hello in different languages.\nSimply select a flag from those available on the screen and the app will show you the word Hello in the selected language.\nYou can also show or hide the pronunciation of the word in the lower left corner of the app.");
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
